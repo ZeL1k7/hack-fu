4e39b5eba61a8d7d8de5aa540593de14f2db9c9b
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2022-11-26 19:38:42 +0300

    fixed bug with tokenizing letters

--- src/models/train_word2vec.py
+++ src/models/train_word2vec.py
@@ -1,6 +1,9 @@
 from gensim.models import Word2Vec
 import pandas as pd
 import numpy as np
+import gensim
+
+from gensim.models import KeyedVectors
 
 
 class WordVec:
@@ -19,7 +22,7 @@ class WordVec:
     def train(self, vocab_data, train_data, save_model: bool = True):
         w2v_model = self.w2v_model
         w2v_model.build_vocab(vocab_data)
-        w2v_model.train(train_data, total_examples=w2v_model.corpus_count, epochs=50, report_delay=1)
+        w2v_model.train(train_data, total_examples=w2v_model.corpus_count, epochs=1, report_delay=1)
         if save_model:
             w2v_model.save("word2vec.model")
 
@@ -33,10 +36,9 @@ if __name__ == "__main__":
     print("droped columns")
     data = np.append(data_unlabeled, data_labeled)
     data = [str(data_) for data_ in data]
-    data = [item for sublist in data for item in sublist.split()]
+    data = [data_.split(' ') for data_ in data]
     print("data loaded")
     w2v = WordVec()
     print('w2v inited')
     w2v.train(data, data)
     print('w2v trained')
-
